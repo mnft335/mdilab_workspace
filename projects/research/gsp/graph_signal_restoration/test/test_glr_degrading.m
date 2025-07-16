8eb56be93ad85199567a968aab6bf0c73c2b5b71
mnft335 <yu_tu.m_03@outlook.jp>
2025-07-16 22:50:54 +0900

    29th commit

--- projects/research/gsp/graph_signal_restoration/test/test_glr_degrading.m
+++ projects/research/gsp/graph_signal_restoration/test/test_glr_degrading.m
@@ -1,6 +1,15 @@
 clear;
 
+num_configurations = numel(generate_weights) * numel(corrupt_weights) * numel(masking_rate) * numel(sigma);
+glr_solved(num_configurations) = 
+
+experiment_config.generate_weights = generate_weights{i}
+experiment_config.corrupt_weights = 
+experiment_config.masking_rate = 
+experiment_config.sigma = 
+
 glr_config = struct();
 shared_config = shared_config_factory(experiment_config);
 
 glr_solved = solve_glr(shared_config, glr_config);
+

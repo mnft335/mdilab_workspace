c4eb05aba3c1c2f9f65f235a3aae574d464e3384
mnft335 <yu_tu.m_03@outlook.jp>
2025-07-25 13:03:37 +0900

    39th commit

--- projects/research/gsp/graph_signal_restoration/developed/RandomFunction.m
+++ projects/research/gsp/graph_signal_restoration/developed/RandomFunction.m
@@ -2,15 +2,7 @@ classdef RandomFunction
 
     properties
 
-        % Top level functions
-        level{1} = containers.Map("KeyType", "char", "ValueType", "any");
-        level{1}("top_level") = {"generate_weights", "generate_signal", "corrupt_weights", "generate_signal_mask"};
 
-        level{2} = containers.Map("KeyType", "char", "ValueType", "any");
-        level{2}("generate_weights") = {"generate_normal_weights", "generate_truncated_normal_weights", "generate_uniform_weights"};
-        level{2}("generate_signal") = {};
-        level{2}("corrupt_weights") = {"add_normal_noise", "add_truncated_normal_noise", "multiply_normal_noise", "multiply_truncated_normal_noise"};
-        level{2}("generate_signal_mask") = {};
         
     end
 
@@ -18,8 +10,33 @@ classdef RandomFunction
 
         function obj = RandomFunction(master_seed)
 
-            for i = 1:numel{level}
+            % % Set up the root and its single child of a function-name tree
+            % obj.functions{1} = containers.Map("KeyType", "char", "ValueType", "any");
+            % obj.functions{1}("root") = {"top_level_functions"};
 
-                key = keys(level{i});
-                seeds = randi(intmax, numel(key), 1);
-                
+            % Set the child functions
+            obj.functions{1} = containers.Map("KeyType", "char", "ValueType", "any");
+            obj.functions{1}("top_level_functions") = {"generate_weights", "generate_signal", "corrupt_weights", "generate_signal_mask"};
+
+            obj.functions{2} = containers.Map("KeyType", "char", "ValueType", "any");
+            obj.functions{2}("generate_weights") = {"generate_normal_weights", "generate_truncated_normal_weights", "generate_uniform_weights"};
+            % obj.functions{3}("generate_signal") = {};
+            obj.functions{2}("corrupt_weights") = {"add_normal_noise", "add_truncated_normal_noise", "multiply_normal_noise", "multiply_truncated_normal_noise"};
+            % obj.functions{3}("generate_signal_mask") = {};
+
+            % Assign the global seed at the top level
+            obj.seeds{1} = containers.Map("KeyType", "char", "ValueType", "any");
+            obj.seeds{1}("top_level_funcitons") = {master_seed};
+
+            % Set seeds for child functions
+            for i = 1:numel{obj.functions}
+                for j = 1:numel{obj.functions{i}}
+
+                    % Get the keys of an element of 
+                    keys = keys(obj.functions{j});
+                    obj.seeds{i} = containers.Map(keys{:}, randi(intmax, numel(keys), 1));
+
+            end
+        end
+
+    end

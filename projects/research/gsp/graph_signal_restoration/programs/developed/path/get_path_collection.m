eaa3fa99bda12b5e5ae22c70c08345a465b82ee6
mnft335 <yu_tu.m_03@outlook.jp>
2025-08-30 18:36:35 +0900

    49th commit

--- projects/research/gsp/graph_signal_restoration/programs/developed/path/get_path_collection.m
+++ projects/research/gsp/graph_signal_restoration/programs/developed/path/get_path_collection.m
@@ -1,7 +1,7 @@
 function path_collection = get_path_collection()
 
     % Define paths to store data
-    path_collection.true_graph_registry = "projects\research\gsp\graph_signal_restoration\resources\data\result_registry";
+    path_collection.true_graph_registry = "projects\research\gsp\graph_signal_restoration\resources\data\true_graph_registry";
     path_collection.result_registry = "projects\research\gsp\graph_signal_restoration\resources\data\result_registry";
 
 end

d6fe4ff3e18f9e0c39bb32ea6e55eead5690b803
mnft335 <yu_tu.m_03@outlook.jp>
2025-09-10 22:14:32 +0900

    63rd commit

--- projects/research/gsp/graph_signal_restoration/programs/developed/get_optimal_grid_result_over_hyperparameter.m
+++ projects/research/gsp/graph_signal_restoration/programs/developed/get_optimal_grid_result_over_hyperparameter.m
@@ -6,20 +6,12 @@ function optimal_grid_result = get_optimal_grid_result_over_hyperparameter(grid_
     % Compute NMSE over the hyperparameter
     nmse_over_hyperparameter = compute_nmse_from_result(grid_result_over_hyperparameter.result);
 
-    % Find the optimal result
+    % Find the indices of hyperparameters that give the optimal result
     dimension_coefficient_l1 = find_parameter_dimension(grid_result_over_hyperparameter.grid_config_collection, "optimization.coefficient_l1");
     [~, idx_optimal_result] = min(nmse_over_hyperparameter, [], dimension_coefficient_l1);
-    
-    % Extract all results at the optimal index
-    optimal_result = extract_result_at_index(grid_result_over_hyperparameter.result, idx_optimal_result, dimension_coefficient_l1);
-    optimal_grid_result = setfield(grid_result_over_hyperparameter, "result", optimal_result);
-end
 
-function extracted_result = extract_result_at_index(result, idx, dimension)
-    % Create index cell array
-    idx_cell = repmat({':'}, ndims(result), 1);
-    idx_cell{dimension} = idx;
-    
-    % Extract the results
-    extracted_result = result(idx_cell{:});
+    % Extract the optimal result
+    optimal_result = extract_elements(grid_result_over_hyperparameter.result, idx_optimal_result, dimension_coefficient_l1);
+    optimal_grid_result = setfield(grid_result_over_hyperparameter, "result", optimal_result);
+
 end

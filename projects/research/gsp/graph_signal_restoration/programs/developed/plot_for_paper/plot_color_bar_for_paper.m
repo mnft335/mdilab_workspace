195ace2619f663ce8c9d7080351f51c716bc1c54
mnft335 <yu_tu.m_03@outlook.jp>
2025-09-19 14:06:50 +0900

    69th commit

--- projects/research/gsp/graph_signal_restoration/programs/developed/plot_for_paper/plot_color_bar_for_paper.m
+++ projects/research/gsp/graph_signal_restoration/programs/developed/plot_for_paper/plot_color_bar_for_paper.m
@@ -1,3 +1,10 @@
 figure_object = figure;
+height = 2.5;
+set(figure_object, 'Units', 'inches', 'Position', [0, 0, 0.5, height]);
 axes_object = axes(figure_object, "Visible", "off");
-color_bar = colorbar(axes_object, 'eastoutside');
+color_bar = colorbar(axes_object);
+colormap(figure_object, jet);
+caxis(axes_object, [0, 1]);
+
+exportgraphics(figure_object, fullfile("projects\research\gsp\graph_signal_restoration\resources\data\images\color_bar", "color_bar.jpg"));
+exportgraphics(figure_object, fullfile("projects\research\gsp\graph_signal_restoration\resources\data\images\color_bar", "color_bar.eps"), "ContentType", "vector", "BackgroundColor", "none");
